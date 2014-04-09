e136b330e06d127eba71c54b824b58d458fb2507
funkeymonky <publick_eye@mail.ru>
2014-04-09 10:04:48 +0400

    Update test.py

--- test.py
+++ test.py
@@ -16,54 +16,59 @@ def PrepareFileName(fileName):
         pass
     return fileName
 
-# command-line args parsing
-parser = argparse.ArgumentParser(description='This is a MP3 ID3 parsing tool')
+def main():
+    # command-line args parsing
+    parser = argparse.ArgumentParser(description='This is a MP3 ID3 parsing tool')
 
-parser.add_argument('--music-dir', action='store', dest='music_dir',
-                    help='Set the destination directory')
+    parser.add_argument('--music-dir', action='store', dest='music_dir',
+                        help='Set the destination directory')
 
-parser.add_argument('--delete', action='store_true', default=False,
-                    dest='boolean_switch',
-                    help='Set a switch to true')
+    parser.add_argument('--delete', action='store_true', default=False,
+                        dest='is_delete',
+                        help='Set a switch to true')
 
-results = parser.parse_args()
+    results = parser.parse_args()
 
-print 'Destination directory:', results.music_dir
+    results.music_dir = results.music_dir or '.'
 
-if results.boolean_switch:
-    print('The script will delete the file on completion')
-else:
-    print('The script will NOT delete the file on completion')
+    print "Destination directory:", results.music_dir
 
 
-for dirpath, dirnames, filenames in os.walk('.'):
-    for filename in filenames:
-        name, ext = os.path.splitext(filename)
-        if ext == '.mp3':
-            try:
-                id3info = ID3(filename)
-                print id3info
-                print 'Title:', id3info['TITLE']
-                print 'Artist:', id3info['ARTIST']
-                new_name = id3info['ARTIST'] + "-" + id3info['TITLE']
-                new_name = PrepareFileName(new_name)
 
-            except InvalidTagError, message:
-                print "Invalid ID3 tag:", message
+    if results.is_delete:
+        print "The script will delete the file on completion"
+    else:
+        print "The script will NOT delete the file on completion"
 
-            if new_name:
+    for dirpath, dirnames, filenames in os.walk('.'):
+        for filename in filenames:
+            name, ext = os.path.splitext(filename)
+            if ext == '.mp3':
                 try:
-                    # Check if the new_name file exists, rename new_name + "1"
-                    shutil.copy(
-                        os.path.join(dirpath, filename),
-                        os.path.join(dirpath, new_name + ".mp3")
-                    )
-                except:
-                    print "Cannot rename file " + filename
+                    id3info = ID3(filename)
+                    print(id3info)
+                    print "Title:", id3info['TITLE']
+                    print "Artist:", id3info['ARTIST']
+                    new_name = id3info['ARTIST'] + "-" + id3info['TITLE']
+                    new_name = PrepareFileName(new_name)
 
-            if results.boolean_switch:
-                try:
-                    os.unlink(filename)
-                except:
-                    print "Cannot delete file " + filename
+                except InvalidTagError, message:
+                    print("Invalid ID3 tag:", message)
 
+                if new_name:
+                    try:
+                        shutil.copy(
+                            os.path.join(dirpath, filename),
+                            os.path.join(results.music_dir, new_name + ".mp3")
+                        )
+                    except:
+                        print "Cannot rename file", filename
+
+                if results.is_delete:
+                    try:
+                        os.unlink(filename)
+                    except:
+                        print "Cannot delete file", filename
+
+if __name__ == "__main__":
+    main() # will call the 'main' function only when you execute this from the command line.
